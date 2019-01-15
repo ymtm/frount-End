9e746897f997b0bc107c47c3d69c55c0baeed16b
Turki Alomari <t-matar@windowslive.com>
2019-01-15 16:34:10 +0300

    mounting

--- syanah/src/App.js
+++ syanah/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import companies from './components/Companies';
+import clients from './components/Clients';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      compoanie:[],
+
+
+    }
+  }
+
+  componentDidMount(){
+    console.log('fetching data');
+    fetch('http://localhost:3000/Companies')
+    .then( response => response.json())
+    .then( data => {
+      console.log(data);
+      this.setState({
+        companies: data
+      })
+    })
+    .catch (error => {
+      console.log(error)
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  
       </div>
     );
   }
