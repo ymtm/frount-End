69e1b9a85a20d3ed582c06bd82b6bd16fb5ad8d7
muneerabaz <muneerabinbaz@gmail.com>
2019-01-15 17:59:35 +0300

    Version3 muneera

--- syanah/src/App.js
+++ syanah/src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import Clients from './components/Clients';
+import Companies from './components/Companies'; 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  } 
+
+  deleteTheContract(contract){
+    const API_URL= '';
+    const url = API_URL + `/companies/${comp_id}/client/${client_id}`;
+    fetch(url, { method: 'DELETE' })
+      .then(response => response.json())
+      .then(data => {
+         this.state.contract.filter( el => el.id !== contract.id );
+      })
+        try {
+          throw new Error('error in delete contract');
+      }
+      catch(e) {
+          console.log(e);
+      
+      }
+  }
+
   render() {
     return (
       <div className="App">
---
+++ syanah/src/components/Clients.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Clients extends Component{
+    constructor(){
+        super();
+        this.state = {
+        //   hovered: false
+        } 
+    }
+
+    render(){
+        return(
+            <div>
+                all the companies
+            </div>
+        )
+    }
+}
