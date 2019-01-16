3ca372941cec6e33a17b495edae05854a1dc5473
YA7MO <ya7yamo1@gmail.com>
2019-01-16 19:10:02 +0300

    show for the contract but we need to add something to hide the list of companies when the userType is company

--- syanah/src/App.js
+++ syanah/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Companies from './components/Companies';
 import ShowClient from './components/ShowClient';
+import ShowCompany from './components/ShowCompany';
 
 class App extends Component {
   constructor() {
@@ -13,10 +14,10 @@ class App extends Component {
       listOfcomps: true,
       isSelected: false,
       userType: null,
+      contracts:[],
 
     }
   }
-
 
   componentDidMount() {
     console.log('fetching data');
@@ -33,43 +34,7 @@ class App extends Component {
       })
   }
 
-  //  RENDERS
-  //
-  //
-  //
-  //
-  //
-
-
-  renderCompanies(allCompanies) {
-    return allCompanies.map((company) => {
-      return (
-        <Companies key={company.id} userType={this.state.userType} comp={company} renderCompanyContracts={this.renderCompanyContracts.bind(this)} renderCompany={this.renderCompany.bind(this)} />
-      )
-    })
-  }
-
-  renderCompany(id) {
-    this.setState({
-      listOfcomps: false,
-    })
-    console.log('clicked', id);
-    const companyByID = this.state.companies.filter((elem) => {
-      return elem.comp_id === id;
-    });
-    
-    this.setState({
-      thatCompany: companyByID,
-    })
-
-  }
-
-  renderCompanyByID(comp) {
-    console.log('* * * * * ', comp[0]);
-    return <ShowClient thatCompany={comp[0]}/>
-  }
-
-  renderCompanyContracts(id) {
+  getCompanyContracts(id) {
     console.log('fetching data');
     fetch(`http://localhost:3000/companies/show/${id}`)
       .then(response => response.json())
@@ -82,6 +47,54 @@ class App extends Component {
       .catch(error => {
         console.log(error)
       })
+    }
+        getCompany(id) {
+          this.setState({
+            listOfcomps: false,
+          })
+          console.log('clicked', id);
+          const companyByID = this.state.companies.filter((elem) => {
+            return elem.comp_id === id;
+          });
+      
+          this.setState({
+            thatCompany: companyByID,
+          })
+      
+        }
+
+  //  RENDERS
+  //
+  //
+  //
+  //
+  //
+
+
+  renderCompanies(allCompanies) {
+    if (this.state.thatCompany.length === 0) {
+      return allCompanies.map((company) => {
+        return (
+          <Companies key={company.id} userType={this.state.userType} comp={company} getCompanyContracts={this.getCompanyContracts.bind(this)} getCompany={this.getCompany.bind(this)} />
+        )
+      })
+    }
+
+  }
+
+
+  renderCompanyByID(comp) {
+    console.log('* * * * * ', comp[0]);
+    return <ShowClient thatCompany={comp[0]} />
+  }
+
+  renderContracs(contracts){
+    return contracts.map((contract) =>{
+      return (
+        <ShowCompany contract={contract}/>
+      )
+    })
+  
   }
 
 
@@ -164,35 +177,37 @@ class App extends Component {
   //
   //
   //
-
+  //
+  
   setUserTypeToClient() {
-   this.setState({
-     userType : 'client'
-   })
-   
+    this.setState({
+      userType: 'client'
+    })
+
   }
   setUserTypeToCompany() {
-   this.setState({
-     userType : 'company'
-   })
+    this.setState({
+      userType: 'company'
+    })
   }
 
-  
 
-  checkingSelection() {
-    // this will check wheather the selection on the landing page is a client or a company,
-    //and it going to render different stuff based on that selection
 
-  }
+  // checkingSelection() {
+  //   // this will check wheather the selection on the landing page is a client or a company,
+  //   //and it going to render different stuff based on that selection
+
+  // }
 
   render() {
     return (
       <main className="container">
         <button className="btn btn-sm m-2 btn-danger" onClick={() => { this.setUserTypeToClient() }}> client</button>
         <button className="btn btn-sm m-2 btn-primary" onClick={() => { this.setUserTypeToCompany() }}>company</button>
-        
-        {this.state.userType ? this.renderCompanies(this.state.companies) : ''}
 
+        {this.state.userType ? this.renderCompanies(this.state.companies) : ''}
+        {this.state.thatCompany.length !== 0 ? this.renderCompanyByID(this.state.thatCompany) : ''}
+        {this.state.userType === 'company' ? this.renderContracs(this.state.contracts) : ''}
       </main>
 
 
---
+++ syanah/src/components/showCompany.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ShowCompany =  (props) => {
+
+    return(
+        <div>
+            {props.contract.client_name}
+        </div>
+    )
+}
+
+export default ShowCompany
