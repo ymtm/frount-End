dc2583dd81783cb1d8e35cbae64216ff79dd62c4
MayMo7a <maymo7a@gmail.com>
2019-01-16 14:30:09 +0300

    handleChange, handleSubmit for the contractForm in client.js

--- syanah/src/components/Clients.js
+++ syanah/src/components/Clients.js
@@ -6,19 +6,32 @@ class Clients extends Component{
         this.state = {
             name: '',
             type: '',
-            period: null,
+            period: null
 
-        }
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = 
+    }
+
+    handleChange(event){
+        this.setState({
+            value: event.target.name
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log('U R doin somethin!!!')
     }
     
     render(){
         return(
             <div className="modal">
-                <form className="cont_client">
-                <label>Clinet Name:</label><input type="text" value={this.state.name} name="name"/><br/>
-                <label>Building Type:</label> <input type="text" value={this.state.type} name="type"/><br/>
+                <form className="cont_client" onSubmit={this.handleSubmit}>
+                <label>Clinet Name:</label><input type="text" value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+                <label>Building Type:</label> <input type="text" value={this.state.type} name="type" onChange={this.handleChange}/><br/>
                 <label>Contract Period:</label>
-                    <select name="period" value={this.state.period}>
+                    <select name="period" value={this.state.period} onChange={this.handleChange}>
                         <option value="three months">3 months</option>
                         <option value="six months">6 months</option>
                         <option value="one year">1 year</option>
