6c56cfea4fc741f35af63de400dcca9efb029402
MayMo7a <maymo7a@gmail.com>
2019-01-16 13:19:25 +0300

    assign a ftate for the contractForm

--- syanah/src/components/Clients.js
+++ syanah/src/components/Clients.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 
 class Clients extends Component{
+    constructor(){
+        super();
+        this.state = {
+            name: '',
+            type: '',
+            period: null,
 
+        }
+    }
+    
     render(){
         return(
             <div className="modal">
                 <form className="cont_client">
                 <label>Clinet Name:</label><input type="text" value={this.state.name} name="name"/><br/>
-                <label>Building Type:</label> <input type="text" value={this.state.date} name="building type"/><br/>
+                <label>Building Type:</label> <input type="text" value={this.state.type} name="type"/><br/>
                 <label>Contract Period:</label>
                     <select name="period" value={this.state.period}>
                         <option value="three months">3 months</option>
