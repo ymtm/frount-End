d85b7124b4ba411219eb0e4e601d4a0ea63a100d
MayMo7a <maymo7a@gmail.com>
2019-01-16 11:55:11 +0300

    finished with contract form!

--- syanah/src/components/Clients.js
+++ syanah/src/components/Clients.js
@@ -5,6 +5,19 @@ class Clients extends Component{
     render(){
         return(
             <div className="modal">
+                <form className="cont_client">
+                <label>Clinet Name:</label><input type="text" value={this.state.name} name="name"/><br/>
+                <label>Building Type:</label> <input type="text" value={this.state.date} name="building type"/><br/>
+                <label>Contract Period:</label>
+                    <select name="period" value={this.state.period}>
+                        <option value="three months">3 months</option>
+                        <option value="six months">6 months</option>
+                        <option value="one year">1 year</option>
+                    </select><br/>
+                <button>submit</button>
+        
+                
+                </form>
             
             </div>
         )
