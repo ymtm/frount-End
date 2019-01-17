0b22da75700b04a20106c165395ca57fe7cda54d
YA7MO <ya7yamo1@gmail.com>
2019-01-17 10:24:16 +0300

    added a condition so the list of companies does not appear when the usertype is company

--- syanah/src/components/showCompany.js
+++ syanah/src/components/showCompany.js
@@ -3,8 +3,12 @@ import React from 'react'
 const ShowCompany =  (props) => {
 
     return(
-        <div>
+        <div> 
             {props.contract.client_name}
+            {props.contract.client_type}
+            {props.contract.contract_period}
+            {props.contract.contract_status}
+            {props.contract.contract_issue_date}
         </div>
     )
 }
